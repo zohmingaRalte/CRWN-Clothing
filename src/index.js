f05075a1fb600311754221ab7607874974e1f57a
Zohminga <zohminga@pop-os.localdomain>
2021-05-14 01:23:23 +0530

    update our menu-items to navigate withRouter HOC

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 // import { Provider } from "react-redux";
 // import { PersistGate } from "redux-persist/integration/react";
 
@@ -11,11 +11,12 @@ import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
-    {/* <Router> */}
-    {/* <PersistGate persistor={persistor}> */}
-    <App />
-    {/* </PersistGate> */}
-    {/* // </Router> */}
+    <Router>
+      {/* <PersistGate persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
+      //{" "}
+    </Router>
     {/* // </Provider> */}
   </React.StrictMode>,
 
