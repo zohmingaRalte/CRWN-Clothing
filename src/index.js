0f7e7d66c932c4d20b96922128b1bd3efd1be225
Zohminga <zohminga@pop-os.localdomain>
2021-05-14 18:18:37 +0530

    added redux, and implemented userReducer and userActions to header and app components

--- src/index.js
+++ src/index.js
@@ -3,21 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 // import { PersistGate } from "redux-persist/integration/react";
 
 // import { store, persistor } from "./redux/store";
+import store from "./redux/store";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <Router>
-      {/* <PersistGate persistor={persistor}> */}
-      <App />
-      {/* </PersistGate> */}
-      {/* {" "} */}
-    </Router>
-    {/* // </Provider> */}
+    <Provider store={store}>
+      <Router>
+        {/* <PersistGate persistor={persistor}> */}
+        <App />
+        {/* </PersistGate> */}
+        {/* {" "} */}
+      </Router>
+    </Provider>
   </React.StrictMode>,
 
   document.getElementById("root")
