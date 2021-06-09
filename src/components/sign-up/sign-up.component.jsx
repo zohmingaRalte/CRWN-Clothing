44fa236bd4e6d222f4d116c2650e5112ab71d410
Zohminga <zohminga@pop-os.localdomain>
2021-06-09 23:01:19 +0530

    back to history

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -37,7 +37,7 @@ const SignUp = ({ signUpStart }) => {
 
   return (
     <SignUpContainer>
-      <SignUpTitle>I do not have a account</SignUpTitle>
+      <SignUpTitle>I do not have an account</SignUpTitle>
       <span>Sign up with your email and password</span>
       <form className="sign-up-form" onSubmit={handleSubmit}>
         <FormInput
@@ -45,7 +45,7 @@ const SignUp = ({ signUpStart }) => {
           name="displayName"
           value={displayName}
           onChange={handleChange}
-          label="Display Name"
+          label="User Name"
           required
         />
         <FormInput
